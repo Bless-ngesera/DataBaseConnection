d5467bad7b8ea7400df2c4ee4d72b3bce90756ca
Bless-ngesera <164491766+Bless-ngesera@users.noreply.github.com>
2025-03-22 11:37:48 +0200

    Delete GUI directory

--- GUI/library_db.sql
+++
@@ -1,9 +0,0 @@
-create database library_db;
-use library_db;
-CREATE TABLE books (
-    bookId INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    author VARCHAR(255) NOT NULL,
-    genre VARCHAR(255) NOT NULL,
-    availability BOOLEAN NOT NULL
-);
